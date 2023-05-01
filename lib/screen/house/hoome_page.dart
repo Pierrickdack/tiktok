7719856b1c9b9e776b9a80c0a1cffd5a4b336dc3
Pierrick <99531267+Pierrickdack@users.noreply.github.com>
2023-05-01 03:09:43 +0100

    Add files via upload

--- lib/screen/house/hoome_page.dart
+++ lib/screen/house/hoome_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HoomePage extends StatelessWidget {
-  HoomePage({Key? key}) : super(key: key);
-
-  
+  const HoomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
